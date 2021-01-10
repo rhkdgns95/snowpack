31b5929e507669f7d2dd7f940a7e3fc48ee5ef32
rhkdgns95 <rhkdgns9489@gmail.com>
2021-01-10 10:44:44 +0900

    Update

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,79 @@
+const antdModules = [
+  'affix',
+  'anchor',
+  'auto-complete',
+  'alert',
+  'avatar',
+  'back-top',
+  'badge',
+  'breadcrumb',
+  'button',
+  'calendar',
+  'card',
+  'collapse',
+  'carousel',
+  'cascader',
+  'checkbox',
+  'col',
+  'comment',
+  'config-provider',
+  'date-picker',
+  'descriptions',
+  'divider',
+  'dropdown',
+  'drawer',
+  'empty',
+  'form',
+  'grid',
+  'input',
+  'image',
+  'input-number',
+  'layout',
+  'list',
+  'message',
+  'menu',
+  'mentions',
+  'modal',
+  'statistic',
+  'notification',
+  'page-header',
+  'pagination',
+  'popconfirm',
+  'popover',
+  'progress',
+  'radio',
+  'rate',
+  'result',
+  'row',
+  'select',
+  'skeleton',
+  'slider',
+  'space',
+  'spin',
+  'steps',
+  'switch',
+  'table',
+  'transfer',
+  'tree',
+  'tree-select',
+  'tabs',
+  'tag',
+  'time-picker',
+  'timeline',
+  'tooltip',
+  'typography',
+  'upload',
+  'version',
+]
+
+function getInstallModules(arr) {
+  const esComponents = arr.map(item => `antd/es/${item}`);
+  const esStyles = arr.map(item => `antd/es/${item}/style/css`);
+  return [
+    ...esComponents,
+    ...esStyles,
+  ]
+}
 module.exports = {
   plugins: [
     "@snowpack/plugin-babel", // 👈 have to add babel here
@@ -9,17 +85,17 @@ module.exports = {
   },
   install: [
     "react/jsx-runtime",
-    "antd/**/*.*"
+    ...getInstallModules(antdModules)
   ],
   mount: {
     public: "/",
     src: "/build",
   },
-  experiments: {
-    optimize: {
-      bundle: true,
-      minify: true,
-      target: "es2018",
-    },
-  },
+  // experiments: {
+  //   optimize: {
+  //     bundle: true,
+  //     minify: true,
+  //     target: "es2018",
+  //   },
+  // },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Button, Space } from "antd";
-
+import { Button, Space, Form, Input, Modal } from "antd";
 export const App = () => {
   console.log("Hello world");
   return (
     <div>
       <h1>hello world</h1>
+      <Modal>
+        <div>dzdz</div>
+      </Modal>
       <Space></Space>
       <Button type="primary">클릭!</Button>
+      <Form>
+        <Form.Item>
+          <Input />
+        </Form.Item>
+      </Form>
     </div>
   );
 };
